fcd749617fedbdc7cafc7c3d55f6666f42dbedd5
Zachary Rock <zacharyrock@hotmail.com>
2014-10-04 23:15:40 -0700

    BUGFIX - All enemies giving 2 points

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -63,10 +63,14 @@ public class Ball : MonoBehaviour {
 				audioSource.Play();
 				Destroy(Instantiate(hitParticle, col.contacts[0].point, Quaternion.identity), 4f); //TODO Eric: change to a better method
 
+				Transform parent = col.transform;
+				while(parent.name != "Enemy(Clone)")
+					parent = parent.parent;
 
 				int pointsToAdd = 1;
-				if(col.transform.GetChild(2).gameObject.activeSelf)
+				if(parent.GetChild(2).gameObject.activeInHierarchy) {
 					pointsToAdd++;
+				}
 
 				if(color == PlayerColor.Red ) {
 					FloatingTextManager.instance.CreateFloatingText( col.transform.position, pointsToAdd, Color.red );
